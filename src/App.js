74b3197e9d31cf7ed711f2781a7480f656c487f4
unknown <wzhuang1324@gmail.com>
2018-09-01 21:56:55 -0700

    Update website title.

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ class App extends Component {
     return (
       <ParallaxProvider>
         <Helmet>
+          <title>Selena Kim</title>
           <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
         </Helmet>
         <div className="App">
